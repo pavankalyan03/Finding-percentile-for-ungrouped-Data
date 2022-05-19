3b59dde4674414e394890d9b1802c37e2c4202b2
Prince Pavan <68956154+pavankalyan03@users.noreply.github.com>
2022-05-19 20:01:19 +0530

    Create percentile.py

---
+++ percentile.py
@@ -0,0 +1,26 @@
+print("perecintile")
+l=list(map(int,input().split()))
+l.sort()
+a=int(input("required percentile: "))
+
+
+def percentile(a):
+
+    z=len(l)
+    x=(z+1)*(a/100)
+    intpart,decpart=int(x),x-int(x)
+    m=l[intpart-1]
+    n=l[intpart]-l[intpart-1]
+    percentile=m+(decpart*n)
+    return percentile
+
+
+def qrt_dev():
+    q=percentile(75)
+    r=percentile(25)
+    y=(q-r)/2
+    return y
+
+
+print("required percentile is: ",percentile(a))
+print("quartile deviation is: ",qrt_dev())
